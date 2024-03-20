3187f0d290208ee3634da5b6e5d856dd4f7a3044
Jorge Ferreyra <jorxfer@gmail.com>
2024-03-20 17:00:05 -0300

    Adding responsive rules

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    screens: {
+      firstq: {'max': '1615px'},
+      secondq: {'max': '1420px'},
+      thirdq: {'max': '1127px'},
+      fourthq: {'max': '915px'},
+      mobile: {'max': '500px'}
+    },
+    extend: {}
+  },
+  plugins: []
 }

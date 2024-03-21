27ebb1c259d48ca09d0ab3804de9e51c3879a91c
Jorge Ferreyra <jorxfer@gmail.com>
2024-03-21 09:21:47 -0300

    Refactoring page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,11 +3,11 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     screens: {
-      firstq: {'max': '1615px'},
-      secondq: {'max': '1420px'},
-      thirdq: {'max': '1127px'},
-      fourthq: {'max': '915px'},
-      mobile: {'max': '500px'}
+      firstq: { 'max': '1615px' },
+      secondq: { 'max': '1420px' },
+      thirdq: { 'max': '1127px' },
+      fourthq: { 'max': '915px' },
+      mobile: { 'max': '500px' }
     },
     extend: {}
   },
